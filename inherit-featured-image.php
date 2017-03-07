fe8a2f69f36d909e422043524dbeda8068f106ec
JS Morisset <jsm@surniaulula.com>
2017-03-06 18:33:33 -0800

    v1.0.1-1

--- inherit-featured-image.php
+++ inherit-featured-image.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: A WordPress plugin to inherit the featured image of the Post / Page parent, grand-parent, etc.
  * Requires At Least: 3.8
- * Tested Up To: 4.7.2
+ * Tested Up To: 4.7.3
  * Version: 1.0.1-1
  * 
  * Version Components: {major}.{minor}.{bugfix}-{stage}{level}
