38aca20906399d8d32d4d2f08246fda0fe2c075b
JS Morisset <jsm@surniaulula.com>
2018-06-28 09:34:12 -0600

    v1.1.1

--- inherit-featured-image.php
+++ inherit-featured-image.php
@@ -13,7 +13,7 @@
  * Requires PHP: 5.4
  * Requires At Least: 3.8
  * Tested Up To: 4.9.6
- * Version: 1.1.0
+ * Version: 1.1.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -36,7 +36,9 @@ if ( ! class_exists( 'InheritFeaturedImage' ) ) {
 		private static $instance;
 
 		public function __construct() {
+
 			add_action( 'plugins_loaded', array( __CLASS__, 'load_textdomain' ) );
+
 			add_filter( 'get_post_metadata', array( __CLASS__, 'get_post_inherited' ), 10, 4 );
 		}
 
@@ -53,37 +55,54 @@ if ( ! class_exists( 'InheritFeaturedImage' ) ) {
 			load_plugin_textdomain( 'inherit-featured-image', false, 'inherit-featured-image/languages/' );
 		}
 
+		/**
+		 * Hooked to the 'get_post_metadata' filter.
+		 */
 		public static function get_post_inherited( $meta_data, $object_id, $meta_key, $single ) {
 			return self::get_inherited( 'post', $meta_data, $object_id, $meta_key, $single );
 		}
 
 		private static function get_inherited( $meta_type, $meta_data, $object_id, $meta_key, $single ) {
+
 			if ( isset( $meta_key ) && $meta_key === '_thumbnail_id' ) {
+
 				$meta_cache = self::get_meta_cache( $object_id, $meta_type );
-				// if there's no featured image, check the parents
+
+				/**
+				 * If there's no featured image, check the parents.
+				 */
 				if ( ! isset( $meta_cache[$meta_key] ) ) {
-					// check each parent, return immediately if a featured image is found
+
+					/**
+					 * Check each parent, return immediately if a featured image is found.
+					 */
 					foreach ( get_post_ancestors( $object_id ) as $parent_id ) {
+
 						$meta_cache = self::get_meta_cache( $parent_id, $meta_type );
+
 						if ( isset( $meta_cache[$meta_key] ) ) {
 							if ( $single ) {
 								return maybe_unserialize( $meta_cache[$meta_key][0] );
 							} else {
-								return array_map('maybe_unserialize', $meta_cache[$meta_key]);
+								return array_map( 'maybe_unserialize', $meta_cache[$meta_key] );
 							}
 						}
 					}
 				}
 			}
+
 			return $meta_data;
 		}
 
 		private static function get_meta_cache( $object_id, $meta_type ) {
+
 			$meta_cache = wp_cache_get( $object_id, $meta_type . '_meta' );
+
 			if ( ! $meta_cache ) {
 				$meta_cache = update_meta_cache( $meta_type, array( $object_id ) );
 				$meta_cache = $meta_cache[$object_id];
 			}
+
 			return $meta_cache;
 		}
 	}
