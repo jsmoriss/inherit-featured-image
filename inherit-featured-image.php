9966f6ebc579fee874cc22bef6104dea6f5f0d10
JS Morisset <jsm@surniaulula.com>
2019-06-26 06:25:38 -0700

    v1.1.1

--- inherit-featured-image.php
+++ inherit-featured-image.php
@@ -12,7 +12,7 @@
  * Description: Inherit the featured image from the Post, Page, or Custom Post Type parent, grand-parent, etc.
  * Requires PHP: 5.6
  * Requires At Least: 3.8
- * Tested Up To: 5.2.1
+ * Tested Up To: 5.2.2
  * Version: 1.1.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
