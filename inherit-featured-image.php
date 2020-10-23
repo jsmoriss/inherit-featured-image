396223ce739ef0a10fec225a5a02d15f8fd85117
JS Morisset <jsm@surniaulula.com>
2020-10-23 09:30:53 -0700

    v1.3.0

--- inherit-featured-image.php
+++ inherit-featured-image.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'InheritFeaturedImage' ) ) {
 
         class InheritFeaturedImage {
 
-		private static $instance = null;
+		private static $instance = null;	// InheritFeaturedImage class object.
 
 		public function __construct() {
 
