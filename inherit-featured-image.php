660df9d30bbb47cadec93ea63bf288b6f65f907c
JS Morisset <jsm@surniaulula.com>
2024-01-06 08:06:32 -0800

    v2.1.1

--- inherit-featured-image.php
+++ inherit-featured-image.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Inherit the featured image from the Post, Page, or Custom Post Type parent, grand-parent, etc.
  * Requires PHP: 7.2.34
- * Requires At Least: 5.5
+ * Requires At Least: 5.8
  * Tested Up To: 6.4.2
  * Version: 2.1.1
  *
